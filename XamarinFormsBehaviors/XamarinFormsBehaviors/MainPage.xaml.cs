f66ee71839abc0dcb3f58f845461c4cc68f7cddf
Houssem Dellai <houssem.dellai@live.com>
2017-03-25 14:06:24 +0100

    Add project files.

---
+++ XamarinFormsBehaviors/XamarinFormsBehaviors/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsBehaviors
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        //{
+        //    var vm = BindingContext as MainViewModel;
+        //    vm.SelectCommand.Execute(null);
+        //}
+    }
+}
---
+++ XamarinFormsBehaviors/XamarinFormsBehaviors/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinFormsBehaviors
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _selectedProduct;
+        private string _purchasedProducts = "You purchased: ";
+
+        public List<string> Products => new List<string>
+        {
+            "Computer", "Smartphone", "Tablet", "Keyboard", "Mouse", "Screen", "Headset"
+        };
+
+        public string SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PurchasedProducts
+        {
+            get { return _purchasedProducts; }
+            set
+            {
+                _purchasedProducts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SelectCommand => new Command(() =>
+        {
+            PurchasedProducts += SelectedProduct + ", ";
+        });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
